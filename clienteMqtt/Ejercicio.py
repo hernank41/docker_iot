352080fbcf419e265d1bb6e2fc0c5068ce468e83
Hernan Kisiel <hernankisiel@example.com>
2024-04-25 15:12:58 -0300

    cosas

--- clienteMqtt/Ejercicio.py
+++ clienteMqtt/Ejercicio.py
@@ -4,9 +4,12 @@ import aiomqtt
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 async def topic0():
-    await client.subscribe(os.environ['TOPICO'])
-    async for message in client.messages:
-        logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+    while True:
+        await client.subscribe(os.environ['TOPICO'])
+        async for message in client.messages:
+            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+    return cont
+    
 
 async def topic1():
     await client.subscribe(os.environ['TOPIC1'])
@@ -35,11 +38,12 @@ async def main():
         port=8883,
         tls_context=tls_context,
     ) as client:
-        task0 = asyncio.create_task(topic0())
-        task1 = asyncio.create_task(topic1())
-        task2 = asyncio.create_task(contar(cont))
-        task3 = asyncio.create_task(publicar(cont))
-        
+        #task0 = asyncio.create_task(topic0())
+        await client.subscribe(os.environ['TOPIC1'])
+        #task2 = asyncio.create_task(contar(cont))
+        #task3 = asyncio.create_task(publicar(cont))
+        async for message in client.messages:
+            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
     asyncio.run(main())
