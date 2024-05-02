6a866d79b05342972ee362d8b5e41dc8c299dd87
Hernan Kisiel <Kisiel@example.com>
2024-05-02 09:27:41 -0300

    print asyncrono

---
+++ clienteMqtt/prueba.py
@@ -0,0 +1,29 @@
+import asyncio
+
+class contador:
+    def __init__(self):
+        self.valor = 0
+    
+    def aumentar(self):
+        self.valor += 1
+
+async def publicar(cont):
+    while True:
+        print(contador1.valor)
+        await asyncio.sleep(5)
+
+async def contar():
+    while True:
+        contador1.aumentar()
+        await asyncio.sleep(3)
+    return cont
+
+async def main():
+
+    async with asyncio.TaskGroup() as Grupo_1:
+        task1 = Grupo_1.create_task((contar()))
+        task2 = Grupo_1.create_task(publicar(contador))
+
+contador1 = contador()
+if __name__ == "__main__":
+    asyncio.run(main())
