88422bd4104176770d3c64274344341d7cb02acf
Hernan Kisiel <Kisiel@example.com>
2024-05-02 09:47:49 -0300

    rekiminar cosas viejas

--- clienteMqtt/prueba.py
+++ clienteMqtt/prueba.py
@@ -7,7 +7,7 @@ class contador:
     def aumentar(self):
         self.valor += 1
 
-async def publicar(cont):
+async def publicar():
     while True:
         print(contador1.valor)
         await asyncio.sleep(5)
@@ -16,13 +16,12 @@ async def contar():
     while True:
         contador1.aumentar()
         await asyncio.sleep(3)
-    return cont
 
 async def main():
 
     async with asyncio.TaskGroup() as Grupo_1:
         task1 = Grupo_1.create_task((contar()))
-        task2 = Grupo_1.create_task(publicar(contador))
+        task2 = Grupo_1.create_task(publicar())
 
 contador1 = contador()
 if __name__ == "__main__":
