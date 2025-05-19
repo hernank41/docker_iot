c36d99c6d98c4a5bf31b3bad25b558ee18ced8de
Germán Xander <german.xander@fio.unam.edu.ar>
2025-05-19 16:12:36 -0300

    usando context.application.handlers

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -1,18 +1,33 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-import logging, os
+import logging, os, aiomysql
 
 token=os.environ["TB_TOKEN"]
-autorizados=[int(x) for x in os.environ["TB_AUTORIZADOS"].split(',')]
 
 logging.basicConfig(format='%(asctime)s - TelegramBot - %(levelname)s - %(message)s', level=logging.INFO)
 
 async def sin_autorizacion(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info("intento de conexión de: " + str(update.message.from_user.id))
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="no autorizado")
+    logging.info(context.application.handlers[0][0].filters.inv_filter.user_ids)
+    sql = "SELECT telegram_id FROM autorizados"
+    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
+                                    user=os.environ["MARIADB_USER"],
+                                    password=os.environ["MARIADB_USER_PASS"],
+                                    db=os.environ["MARIADB_DB"])
+    async with conn.cursor() as cur:
+        await cur.execute(sql)
+        r = await cur.fetchall()
+        context.application.handlers[0][0].filters.inv_filter.user_ids = set([int(row[0]) for row in r])
+        if update.effective_chat.id in context.application.handlers[0][0].filters.inv_filter.user_ids:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="ahora está autorizado")
+            await start(update, context)
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="no autorizado")
+    await cur.close()
+    conn.close()
+    logging.info(context.application.handlers[0][0].filters.inv_filter.user_ids)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info(update)
     logging.info("se conectó: " + str(update.message.from_user.id))
     if update.message.from_user.first_name:
         nombre=update.message.from_user.first_name
@@ -29,9 +44,8 @@ async def acercade(update: Update, context):
     await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
 
 def main():
-    logging.info(autorizados)
     application = Application.builder().token(token).build()
-    application.add_handler(MessageHandler((~filters.User(autorizados)), sin_autorizacion))
+    application.add_handler(MessageHandler(~ filters.User(), sin_autorizacion))
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('acercade', acercade))
     application.run_polling()
