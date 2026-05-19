fef303f1cd42657861e2246478e984b8aed77e9c
Germán Xander <german.xander@fio.unam.edu.ar>
2026-05-19 09:49:34 -0300

    socket como solo lectura

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -10,38 +10,51 @@ async def main():
     tls_context.check_hostname = True
     tls_context.load_default_certs()
 
-    async with aiomqtt.Client(
-        os.environ["SERVIDOR"],
-        username=os.environ["MQTT_USR"],
-        password=os.environ["MQTT_PASS"],
-        port=int(os.environ["PUERTO_MQTTS"]),
-        tls_context=tls_context,
-    ) as client:
-        await client.subscribe(os.environ['TOPICO'])
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-            dispositivo=str(message.topic).split('/')[-1]
-            datos=json.loads(message.payload.decode('utf8'))
-            sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `humedad`) VALUES (%s, %s, %s)"
-            try:
-                conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                            user=os.environ["MARIADB_USER"],
-                                            password=os.environ["MARIADB_USER_PASS"],
-                                            db=os.environ["MARIADB_DB"])
-            except Exception as e:
-                logging.error(traceback.format_exc())
-
-            cur = await conn.cursor()
-
-            async with conn.cursor() as cur:
+    try:
+        async with aiomqtt.Client(
+            os.environ["SERVIDOR"],
+            # username=os.environ["MQTT_USR"],
+            # password=os.environ["MQTT_PASS"],
+            port=int(os.environ["PUERTO_MQTTS"]),
+            tls_context=tls_context,
+            identifier="mi-cliente-001",
+        ) as client:
+            await client.subscribe(os.environ['TOPICO'])
+            async for message in client.messages:
+                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+                dispositivo=str(message.topic).split('/')[-1]
+                datos=json.loads(message.payload.decode('utf8'))
+                sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `humedad`) VALUES (%s, %s, %s)"
                 try:
-                    await cur.execute(sql, (dispositivo, datos['temperatura'], datos['humedad']))
-                    await conn.commit()
-                    await cur.close()
-                    await conn.ensure_closed()
+                    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
+                                                user=os.environ["MARIADB_USER"],
+                                                password=os.environ["MARIADB_USER_PASS"],
+                                                db=os.environ["MARIADB_DB"])
                 except Exception as e:
                     logging.error(traceback.format_exc())
-                    # Logs the error appropriately. 
+
+                cur = await conn.cursor()
+
+                async with conn.cursor() as cur:
+                    try:
+                        await cur.execute(sql, (dispositivo, datos['temperatura'], datos['humedad']))
+                        await conn.commit()
+                        await cur.close()
+                        await conn.ensure_closed()
+                    except Exception as e:
+                        logging.error(traceback.format_exc())
+                        # Logs the error appropriately. 
+    except aiomqtt.MqttError as e:
+        # Error general de MQTT (conexión, suscripción, etc.)
+        print(f"Error MQTT: {e}")
+    except ssl.SSLError as e:
+        print(f"Error SSL/TLS: {e}")
+    except ConnectionError as e:
+        print(f"Error de red: {e}")
+    except asyncio.TimeoutError:
+        print("Timeout en conexión o envío")
+    except Exception as e:
+        print(f"Otro error: {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
